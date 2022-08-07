7e4fde7b83b91101b43957c3964c0683f9facb06
Мигранов Салават Вагизович <SMigranov@citros.ru>
2022-08-07 09:23:42 +0300

    PROGPER-5130: Константы с мапперов вынесены

--- TestProject1/src/main/java/com/example/testproject1/dao/person/mapper/PersonMapper.java
+++ TestProject1/src/main/java/com/example/testproject1/dao/person/mapper/PersonMapper.java
@@ -29,19 +29,26 @@ public class PersonMapper implements RowMapper<Person> {
      */
     @Autowired
     private JobTittleMapper jobTittleMapper;
+    private final String PERSON_ID="person_id";
+    private final String PERSON_FIRST_NAME="person_first_name";
+    private final String PERSON_SECOND_NAME="person_second_name";
+    private final String PERSON_LAST_NAME="person_last_name";
+    private final String PERSON_PHOTO="person_photo";
+    private final String PERSON_PHONE_NUMBER="person_phone_number";
+    private final String PERSON_BIRTH_DAY="person_birth_day";
     /**
      * {@inheritDoc}
      */
     @Override
     public Person mapRow(ResultSet rs, int rowNum) throws SQLException {
         Person person = new Person();
-        person.setId(UUID.fromString(rs.getString("person_id")));
-        person.setFirstName(rs.getString("person_first_name"));
-        person.setSecondName(rs.getString("person_second_name"));
-        person.setLastName(rs.getString("person_last_name"));
-        person.setPhoto(rs.getString("person_photo"));
-        person.setPhoneNumber(rs.getString("person_phone_number"));
-        person.setBirthDay((rs.getDate("person_birth_day")));
+        person.setId(UUID.fromString(rs.getString(PERSON_ID)));
+        person.setFirstName(rs.getString(PERSON_FIRST_NAME));
+        person.setSecondName(rs.getString(PERSON_SECOND_NAME));
+        person.setLastName(rs.getString(PERSON_LAST_NAME));
+        person.setPhoto(rs.getString(PERSON_PHOTO));
+        person.setPhoneNumber(rs.getString(PERSON_PHONE_NUMBER));
+        person.setBirthDay((rs.getDate(PERSON_BIRTH_DAY)));
         Department department = departmentMapper.mapRow(rs, rowNum);
         JobTittle jobTittle = jobTittleMapper.mapRow(rs, rowNum);
         person.setDepartment(department);
--- TestProject1/src/main/java/com/example/testproject1/dao/taskdocument/mapper/TaskDocumentMapper.java
+++ TestProject1/src/main/java/com/example/testproject1/dao/taskdocument/mapper/TaskDocumentMapper.java
@@ -26,6 +26,49 @@ public class TaskDocumentMapper implements RowMapper<TaskDocument> {
      */
     @Autowired
     private BaseDocumentMapper baseDocumentMapper;
+
+    private final String TASK_DOCUMENT_OUT_DATE="task_document_out_date";
+    private final String TASK_DOCUMENT_EXEC_PERIOD="task_document_exec_period";
+    private final String TASK_DOCUMENT_SIGN_OF_CONTROL="task_document_sign_of_control";
+    private final String PERSON_RESPONSE_ID="person_response_id";
+    private final String PERSON_RESPONSE_FIRST_NAME="person_response_first_name";
+    private final String PERSON_RESPONSE_SECOND_NAME="person_response_second_name";
+    private final String PERSON_RESPONSE_LAST_NAME="person_response_last_name";
+    private final String PERSON_RESPONSE_PHOTO="person_response_photo";
+    private final String PERSON_RESPONSE_PHONE_NUMBER="person_response_phone_number";
+    private final String PERSON_RESPONSE_BIRTH_DAY="person_response_birth_day";
+    private final String DEPARTMENT_RESPONSE_ID="department_response_id";
+    private final String DEPARTMENT_RESPONSE_FULL_NAME="department_response_full_name";
+    private final String DEPARTMENT_RESPONSE_SHORT_NAME="department_response_short_name";
+    private final String DEPARTMENT_RESPONSE_SUPERVISOR="department_response_supervisor";
+    private final String DEPARTMENT_RESPONSE_CONTACT_NUMBER="department_response_contact_number";
+    private final String ORGANIZATION_RESPONSE_ID="organization_response_id";
+    private final String ORGANIZATION_RESPONSE_FULL_NAME="organization_response_full_name";
+    private final String ORGANIZATION_RESPONSE_SHORT_NAME="organization_response_short_name";
+    private final String ORGANIZATION_RESPONSE_SUPERVISOR="organization_response_supervisor";
+    private final String ORGANIZATION_RESPONSE_CONTACT_NUMBER="organization_response_contact_number";
+    private final String JOB_TITTLE_RESPONSE_ID="job_tittle_response_id";
+    private final String JOB_RESPONSE_NAME="job_response_name";
+    private final String PERSON_CONTROL_ID="person_control_id";
+    private final String PERSON_CONTROL_FIRST_NAME="person_control_first_name";
+    private final String PERSON_CONTROL_SECOND_NAME="person_control_second_name";
+    private final String PERSON_CONTROL_LAST_NAME="person_control_last_name";
+    private final String PERSON_CONTROL_PHOTO="person_control_photo";
+    private final String PERSON_CONTROL_PHONE_NUMBER="person_control_phone_number";
+    private final String PERSON_CONTROL_BIRTH_DAY="person_control_birth_day";
+    private final String DEPARTMENT_CONTROL_ID="department_control_id";
+    private final String DEPARTMENT_CONTROL_FULL_NAME="department_control_full_name";
+    private final String DEPARTMENT_CONTROL_SHORT_NAME="department_control_short_name";
+    private final String DEPARTMENT_CONTROL_SUPERVISOR="department_control_supervisor";
+    private final String DEPARTMENT_CONTROL_CONTACT_NUMBER="department_control_contact_number";
+    private final String ORGANIZATION_CONTROL_ID="organization_control_id";
+    private final String ORGANIZATION_CONTROL_FULL_NAME="organization_control_full_name";
+    private final String ORGANIZATION_CONTROL_SHORT_NAME="organization_control_short_name";
+    private final String ORGANIZATION_CONTROL_SUPERVISOR="organization_control_supervisor";
+    private final String ORGANIZATION_CONTROL_CONTACT_NUMBER="organization_control_contact_number";
+    private final String JOB_TITTLE_CONTROL_ID="job_tittle_control_id";
+    private final String JOB_CONTROL_NAME="job_control_name";
+
     /**
      * {@inheritDoc}
      */
@@ -34,9 +77,9 @@ public class TaskDocumentMapper implements RowMapper<TaskDocument> {
         //Мапим baseDocument к taskDocument
         BaseDocument baseDocument = baseDocumentMapper.mapRow(rs, rowNum);
         TaskDocument taskDocument = new TaskDocument();
-        taskDocument.setOutDate(rs.getTimestamp("task_document_out_date"));
-        taskDocument.setExecPeriod(rs.getString("task_document_exec_period"));
-        taskDocument.setSignOfControl(rs.getBoolean("task_document_sign_of_control"));
+        taskDocument.setOutDate(rs.getTimestamp(TASK_DOCUMENT_OUT_DATE));
+        taskDocument.setExecPeriod(rs.getString(TASK_DOCUMENT_EXEC_PERIOD));
+        taskDocument.setSignOfControl(rs.getBoolean(TASK_DOCUMENT_SIGN_OF_CONTROL));
         taskDocument.setId(baseDocument.getId());
         taskDocument.setName(baseDocument.getName());
         taskDocument.setText(baseDocument.getText());
@@ -46,28 +89,28 @@ public class TaskDocumentMapper implements RowMapper<TaskDocument> {
 
         //Мапим Person(responsible) к taskDocument
         Person response = new Person();
-        response.setId(UUID.fromString(rs.getString("person_response_id")));
-        response.setFirstName(rs.getString("person_response_first_name"));
-        response.setSecondName(rs.getString("person_response_second_name"));
-        response.setLastName(rs.getString("person_response_last_name"));
-        response.setPhoto(rs.getString("person_response_photo"));
-        response.setPhoneNumber(rs.getString("person_response_phone_number"));
-        response.setBirthDay((rs.getDate("person_response_birth_day")));
+        response.setId(UUID.fromString(rs.getString(PERSON_RESPONSE_ID)));
+        response.setFirstName(rs.getString(PERSON_RESPONSE_FIRST_NAME));
+        response.setSecondName(rs.getString(PERSON_RESPONSE_SECOND_NAME));
+        response.setLastName(rs.getString(PERSON_RESPONSE_LAST_NAME));
+        response.setPhoto(rs.getString(PERSON_RESPONSE_PHOTO));
+        response.setPhoneNumber(rs.getString(PERSON_RESPONSE_PHONE_NUMBER));
+        response.setBirthDay((rs.getDate(PERSON_RESPONSE_BIRTH_DAY)));
         Department departmentResponse = new Department();
-        departmentResponse.setId(UUID.fromString(rs.getString("department_response_id")));
-        departmentResponse.setFullName(rs.getString("department_response_full_name"));
-        departmentResponse.setShortName(rs.getString("department_response_short_name"));
-        departmentResponse.setSupervisor(rs.getString("department_response_supervisor"));
-        departmentResponse.setContactNumber(rs.getString("department_response_contact_number"));
+        departmentResponse.setId(UUID.fromString(rs.getString(DEPARTMENT_RESPONSE_ID)));
+        departmentResponse.setFullName(rs.getString(DEPARTMENT_RESPONSE_FULL_NAME));
+        departmentResponse.setShortName(rs.getString(DEPARTMENT_RESPONSE_SHORT_NAME));
+        departmentResponse.setSupervisor(rs.getString(DEPARTMENT_RESPONSE_SUPERVISOR));
+        departmentResponse.setContactNumber(rs.getString(DEPARTMENT_RESPONSE_CONTACT_NUMBER));
         Organization organizationResponse = new Organization();
-        organizationResponse.setId(UUID.fromString(rs.getString("organization_response_id")));
-        organizationResponse.setFullName(rs.getString("organization_response_full_name"));
-        organizationResponse.setShortName(rs.getString("organization_response_short_name"));
-        organizationResponse.setSupervisor(rs.getString("organization_response_supervisor"));
-        organizationResponse.setContactNumber(rs.getString("organization_response_contact_number"));
+        organizationResponse.setId(UUID.fromString(rs.getString(ORGANIZATION_RESPONSE_ID)));
+        organizationResponse.setFullName(rs.getString(ORGANIZATION_RESPONSE_FULL_NAME));
+        organizationResponse.setShortName(rs.getString(ORGANIZATION_RESPONSE_SHORT_NAME));
+        organizationResponse.setSupervisor(rs.getString(ORGANIZATION_RESPONSE_SUPERVISOR));
+        organizationResponse.setContactNumber(rs.getString(ORGANIZATION_RESPONSE_CONTACT_NUMBER));
         JobTittle jobTittleResponse = new JobTittle();
-        jobTittleResponse.setUuid(UUID.fromString(rs.getString("job_tittle_response_id")));
-        jobTittleResponse.setName(rs.getString("job_response_name"));
+        jobTittleResponse.setUuid(UUID.fromString(rs.getString(JOB_TITTLE_RESPONSE_ID)));
+        jobTittleResponse.setName(rs.getString(JOB_RESPONSE_NAME));
         departmentResponse.setOrganization(organizationResponse);
         response.setDepartment(departmentResponse);
         response.setJobTittle(jobTittleResponse);
@@ -75,28 +118,28 @@ public class TaskDocumentMapper implements RowMapper<TaskDocument> {
 
         //Мапим Person(control_person) к taskDocument
         Person controlPerson = new Person();
-        controlPerson.setId(UUID.fromString(rs.getString("person_control_id")));
-        controlPerson.setFirstName(rs.getString("person_control_id_first_name"));
-        controlPerson.setSecondName(rs.getString("person_control_id_second_name"));
-        controlPerson.setLastName(rs.getString("person_control_id_last_name"));
-        controlPerson.setPhoto(rs.getString("person_control_id_photo"));
-        controlPerson.setPhoneNumber(rs.getString("person_control_id_phone_number"));
-        controlPerson.setBirthDay((rs.getDate("person_control_id_birth_day")));
+        controlPerson.setId(UUID.fromString(rs.getString(PERSON_CONTROL_ID)));
+        controlPerson.setFirstName(rs.getString(PERSON_CONTROL_FIRST_NAME));
+        controlPerson.setSecondName(rs.getString(PERSON_CONTROL_SECOND_NAME));
+        controlPerson.setLastName(rs.getString(PERSON_CONTROL_LAST_NAME));
+        controlPerson.setPhoto(rs.getString(PERSON_CONTROL_PHOTO));
+        controlPerson.setPhoneNumber(rs.getString(PERSON_CONTROL_PHONE_NUMBER));
+        controlPerson.setBirthDay((rs.getDate(PERSON_CONTROL_BIRTH_DAY)));
         Department departmentControl = new Department();
-        departmentControl.setId(UUID.fromString(rs.getString("department_control_id")));
-        departmentControl.setFullName(rs.getString("department_control_full_name"));
-        departmentControl.setShortName(rs.getString("department_control_short_name"));
-        departmentControl.setSupervisor(rs.getString("department_control_supervisor"));
-        departmentControl.setContactNumber(rs.getString("department_control_contact_number"));
+        departmentControl.setId(UUID.fromString(rs.getString(DEPARTMENT_CONTROL_ID)));
+        departmentControl.setFullName(rs.getString(DEPARTMENT_CONTROL_FULL_NAME));
+        departmentControl.setShortName(rs.getString(DEPARTMENT_CONTROL_SHORT_NAME));
+        departmentControl.setSupervisor(rs.getString(DEPARTMENT_CONTROL_SUPERVISOR));
+        departmentControl.setContactNumber(rs.getString(DEPARTMENT_CONTROL_CONTACT_NUMBER));
         Organization organizationControl = new Organization();
-        organizationControl.setId(UUID.fromString(rs.getString("organization_control_id")));
-        organizationControl.setFullName(rs.getString("organization_control_full_name"));
-        organizationControl.setShortName(rs.getString("organization_control_short_name"));
-        organizationControl.setSupervisor(rs.getString("organization_control_supervisor"));
-        organizationControl.setContactNumber(rs.getString("organization_control_contact_number"));
+        organizationControl.setId(UUID.fromString(rs.getString(ORGANIZATION_CONTROL_ID)));
+        organizationControl.setFullName(rs.getString(ORGANIZATION_CONTROL_FULL_NAME));
+        organizationControl.setShortName(rs.getString(ORGANIZATION_CONTROL_SHORT_NAME));
+        organizationControl.setSupervisor(rs.getString(ORGANIZATION_CONTROL_SUPERVISOR));
+        organizationControl.setContactNumber(rs.getString(ORGANIZATION_CONTROL_CONTACT_NUMBER));
         JobTittle jobTittleControl = new JobTittle();
-        jobTittleControl.setUuid(UUID.fromString(rs.getString("job_tittle_control_id")));
-        jobTittleControl.setName(rs.getString("job_control_name"));
+        jobTittleControl.setUuid(UUID.fromString(rs.getString(JOB_TITTLE_CONTROL_ID)));
+        jobTittleControl.setName(rs.getString(JOB_CONTROL_NAME));
         departmentControl.setOrganization(organizationControl);
         controlPerson.setDepartment(departmentControl);
         controlPerson.setJobTittle(jobTittleControl);
