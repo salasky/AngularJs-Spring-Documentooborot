cfd8dab73a41ac1a2a54e4cf861438c229567b60
Мигранов Салават Вагизович <SMigranov@citros.ru>
2022-08-10 12:17:58 +0300

    PROGPER-5130: ImportCongiguration добавлен

--- TestProject1/src/main/java/com/example/testproject1/service/importxmltodatabase/Impl/XmlToDataBaseImporterImpl.java
+++ TestProject1/src/main/java/com/example/testproject1/service/importxmltodatabase/Impl/XmlToDataBaseImporterImpl.java
@@ -1,5 +1,6 @@
 package com.example.testproject1.service.importxmltodatabase.Impl;
 
+import com.example.testproject1.configuration.importConfiguration.ImportCongiguration;
 import com.example.testproject1.model.staff.Department;
 import com.example.testproject1.model.staff.JobTittle;
 import com.example.testproject1.model.staff.Organization;
@@ -11,6 +12,7 @@ import com.example.testproject1.service.staffservice.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 /**
  * Класс сохранения данных из XML в базу данных
  *
@@ -19,63 +21,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class XmlToDataBaseImporterImpl implements XmlToDataBaseImporter {
 
-    /**
-     * Бин CrudService Person
-     */
     @Autowired
-    private CrudService<Person> personCrudService;
-    /**
-     * Бин CrudService Department
-     */
-    @Autowired
-    private CrudService<Department> departmentCrudService;
-    /**
-     * Бин CrudService Organization
-     */
-    @Autowired
-    private CrudService<Organization> organizationCrudService;
-    /**
-     * Бин CrudService JobTittle
-     */
-    @Autowired
-    private CrudService<JobTittle> jobTittleCrudService;
-    /**
-     * Бин сервиса для работы с person xml
-     */
-    @Autowired
-    private StorageService<Person> personStorageService;
-    /**
-     * Бин сервиса для работы с Department xml
-     */
-    @Autowired
-    private StorageService<Department> departmentStorageService;
-    /**
-     * Бин сервиса для работы с Organization xml
-     */
-    @Autowired
-    private StorageService<Organization> organizationStorageService;
-    /**
-     * Бин сервиса для работы с Job xml
-     */
-    @Autowired
-    private StorageService<JobTittle> jobStorageService;
-
+    private ImportCongiguration importCongiguration;
     /**
      * {@inheritDoc}
      */
     @Override
     public void saveStaffInDb(){
-        save(organizationStorageService,organizationCrudService);
-        save(jobStorageService,jobTittleCrudService);
-        save(departmentStorageService,departmentCrudService);
-        save(personStorageService,personCrudService);
+        importCongiguration.getStorageServiceMap().forEach(this::save);
     }
 
-
     private void save(StorageService storageService,CrudService crudService){
-        for (Object obj: storageService.getList()
-             ) {
-            crudService.create(obj);
-        }
+
+        storageService.getList().forEach(crudService::create);
     }
 }
