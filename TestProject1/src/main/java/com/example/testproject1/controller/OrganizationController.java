884e61df9decb45e898bd516c8984544ec9af6e6
Мигранов Салават Вагизович <SMigranov@citros.ru>
2022-08-11 12:09:04 +0300

    PROGPER-5131: Создан контроллер организации

---
+++ TestProject1/src/main/java/com/example/testproject1/controller/OrganizationController.java
@@ -0,0 +1,26 @@
+package com.example.testproject1.controller;
+
+import com.example.testproject1.model.staff.Organization;
+import com.example.testproject1.service.dbservice.CrudService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/organizations")
+public class OrganizationController {
+    @Autowired
+    private CrudService<Organization> organizationCrudService;
+    @GetMapping
+    public List<Organization> getAll(){
+        return organizationCrudService.getall();
+    }
+
+}
