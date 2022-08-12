052fb3768b0a9423f22f5505ac58056533363463
Мигранов Салават Вагизович <SMigranov@citros.ru>
2022-08-12 14:51:34 +0300

    PROGPER-5130: XmlImportInDBTest добавлен bug fix

--- TestProject1/src/test/java/com/example/testproject1/XmlImportInDBTest.java
+++ TestProject1/src/test/java/com/example/testproject1/XmlImportInDBTest.java
@@ -11,7 +11,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 
-
 @SpringBootTest
 @TestPropertySource(
         locations = "classpath:application.yaml")
