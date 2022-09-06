be31efe5799936352274a586a8a66c1c24286bfe
Мигранов Салават Вагизович <SMigranov@citros.ru>
2022-09-06 11:50:03 +0300

    PROGPER-5132: убрана лишняя компонента

--- TestProject1/src/main/resources/app/component/split.js
+++ TestProject1/src/main/resources/app/component/split.js
@@ -19,12 +19,3 @@ app.component('splitRight', {
             '        </div>' +
             '       </div>'
     })
-    .component('tab-content', {
-        transclude: true,
-        template: '   <div class="tab-content">\n' +
-            '            <div class="tab-pane" ng-repeat="tab in vm.tabs"' +
-            '                   ng-class="{true:\'active\',false:\'\'}[tab==vm.activeTabNo]">'+
-            '           <ng-transclude></ng-transclude>' +
-            '        </div>' +
-            '       </div>'
-    });
